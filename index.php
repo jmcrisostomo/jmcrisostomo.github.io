7d43fa31e734e98d087b052c08c78d48be2be683
JM Crisostomo <crisjohnmatthew13@gmail.com>
2017-06-23 04:28:00 +0800

    changes

--- index.php
+++ index.php
@@ -7,11 +7,11 @@
 
 <body>
 	<?php
-	require("class-Clockwork.php")
+	require("class-Clockwork.php");
 		$apikey = "e47589474eb317b599bb992007c92587ae2f9462";
 		$clockwork = new Clockwork($apikey);
 		
-		$message = array('to' => '09993339030', 'message' = > 'You did it brah hahaha');
+		$message = array('to' => '+639993339030', 'message' => 'You did it brah hahaha');
 		$done = $clockwork->send($message);
 	?>
 	<h1>It works...I think?</h1>
